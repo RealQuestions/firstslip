f675794f619ba14c08cbb4fcaf9598b7bfb62e5c
Erle Pereira <erle@erlepereira.com>
2014-08-16 16:06:41 +1000

    more fixes

---
+++ profiles/realquestionsau/themes/realquestions/js/donationschart29may.js
@@ -0,0 +1,111 @@
+google.load('visualization', '1', {
+    'callback': 'drawChart29may',
+    packages: [
+        'corechart'
+    ]
+});
+function drawChart29may() {
+    var data = google.visualization.arrayToDataTable([
+    ['State',
+    'Donation $',
+    {
+        role: 'style'
+    }
+    ],
+    [
+        'FED',
+        12400,
+        '#601BC6'
+    ],
+    [
+        'VIC',
+        12400,
+        '#070938'
+    ],
+    [
+        'SA',
+        12400,
+        '#F92121'
+    ],
+    [
+        'TAS',
+        12400,
+        '#004C25'
+    ],
+    [
+        'WA',
+        2100,
+        '#D2AE02'
+    ],
+    [
+        'NT',
+        1500,
+        '#282000'
+    ],
+    [
+        'ACT',
+        1000,
+        '#0C2CA3'
+    ],
+    [
+        'NSW',
+        1000,
+        '#87CEFA'
+    ],
+    [
+        'QLD',
+        12400,
+        '#830300'
+    ],
+    ]);
+    var options = {
+        title: 'Maximum political donation without disclosure - by State',
+        hAxis: {
+            title: 'State or Territory (* Commonwealth not shown in map below)',
+            titleTextStyle: {
+                color: 'black'
+            }
+        }
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div29may'));
+    chart.draw(data, options);
+}
+google.load('visualization', '1', {
+    'callback': 'drawMarkersMap29may',
+    packages: [
+        'corechart'
+    ]
+});
+
+    function drawMarkersMap29may() {
+    var data = google.visualization.arrayToDataTable([
+        ['City',   'Donation $'],
+                ['AU-VIC', 12400 ],
+                ['AU-TAS', 12400],
+                ['AU-NSW', 1000],
+                ['AU-QLD', 12400],
+                ['AU-NT', 1500],
+                ['AU-WA', 2100],
+                ['AU-SA', 12400],
+                ['AU-ACT', 1000]
+          ]);
+    
+     var options = {
+        region: 'AU',
+        width: 900,
+        height: 500,
+        displayMode: 'markers',
+        colorAxis: {colors: ['#9C5DFA', '#290D52']},
+        resolution: 'provinces',
+    };
+                     
+        
+    var chart = new google.visualization.GeoChart(document.getElementById('chart_div129may'));
+        chart.draw(data, options);
+
+    }
+    
+
+
+google.setOnLoadCallback(drawVisualization);
+
